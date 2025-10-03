f3e49373842b5c8d3b361939dcf5b64ce0491ace
QL-Kapil-Kant <kapil.kant@quokkalabs.com>
2025-10-03 11:32:53 +0530

    first commit

---
+++ src/test/java/com/ql/base_java/BaseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ql.base_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
